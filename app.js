6f40b3d80ba0201ccaa593056fe222658564ba9c
mike <mixailted@gmail.com>
2023-01-09 17:33:39 -0800

    fixed   css markUp   code

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ let  get = function() { fetch('https://api.adviceslip.com/advice')
   return response.json();
 })
 .then((data1) => {
-  console.log(data1)
-  document.querySelector('.id').innerHTML = ` ADVICE ID # ${data1.slip.id}`;
-  document.querySelector('.r-id').innerHTML = `${data1.slip.advice}`;
-})}
+  document.querySelector('.text-id').textContent = ` ADVICE ID # ${data1.slip.id}`;
+  document.querySelector('.advizer-text').textContent = `${data1.slip.advice}`;
+})};
 
-get()
-const btn  = document.querySelector('.btn-advice').addEventListener('click', ()=> get.call(get));
+get();
+
+
+const btn  = document.querySelector('.dice').addEventListener('click', ()=> get());
